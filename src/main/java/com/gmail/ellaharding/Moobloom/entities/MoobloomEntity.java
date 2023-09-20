3d93650f354fe9f0decd1fe6c6c6b784fa0475e1
ellaharding <ellaharding@gmail.com>
2023-09-20 13:23:04 +1000

    Mooblooms are tempted by flowers, and can be bred with dandelions - the offspring is a moobloom though, so that needs to be fixed.

--- src/main/java/com/gmail/ellaharding/Moobloom/entities/MoobloomEntity.java
+++ src/main/java/com/gmail/ellaharding/Moobloom/entities/MoobloomEntity.java
@@ -2,7 +2,7 @@ package com.gmail.ellaharding.Moobloom.entities;
 
 import com.gmail.ellaharding.Moobloom.init.EntityInit;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Holder;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobSpawnType;
@@ -10,9 +10,12 @@ import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.animal.Cow;
 import net.minecraft.world.entity.animal.MushroomCow;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.level.biome.Biome;
+
 
 public class MoobloomEntity extends MushroomCow {
 
@@ -39,8 +42,12 @@ public class MoobloomEntity extends MushroomCow {
         this.goalSelector.addGoal(4, new WaterAvoidingRandomStrollGoal(this,1));
         this.goalSelector.addGoal(5, new LookAtPlayerGoal(this, Player.class,6.0f));
         this.goalSelector.addGoal(6, new RandomLookAroundGoal(this));
-        // Fix for tempting by flower this.goalSelector.addGoal(7, new TemptGoal(this,1.0, Ingredient.of(ItemLike.), false));
-        //research brains for more complex AI
+        this.goalSelector.addGoal(7, new TemptGoal(this,1.0, Ingredient.of(ItemTags.FLOWERS), false));
+    }
+    @Override
+    public boolean isFood(ItemStack stack) {
+        // Eventually work out how to make this be any flower type
+        return stack.getItem() == Items.DANDELION;
     }
 
     public static boolean canSpawn(EntityType<MoobloomEntity> entityType,
